447ac5bc4be3caea51108f02bc54e4d68092476e
Neil Roberts <neil.roberts@absa.africa>
2021-02-04 07:59:07 +0200

    Refactored create and edit screens

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,29 +3,44 @@ import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'reac
 import { Context } from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons';
 
-const IndexScreen = () => {
+const IndexScreen = ({ navigation }) => {
 
-  const { state, addBlogPost, deleteBlogPost } = useContext(Context);
+  const { state, deleteBlogPost } = useContext(Context);
 
   return (
     <View>
-      <Button title="Add Post" onPress={() => addBlogPost()} />
       <FlatList
         data={state}
         keyExtractor={(blogPost) => blogPost.title}
         renderItem={({ item }) => {
           return (
-            <View style={styles.row}>
-              <Text style={styles.title}>{item.title} - {item.id}</Text>
-              <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
-              <Feather style={styles.icon} name="trash" />
-              </TouchableOpacity>
-            </View>
+            <TouchableOpacity onPress={() => navigation.navigate('Show', { id: item.id })}>
+              <View style={styles.row}>
+                <Text style={styles.title}>{item.title} - {item.id}</Text>
+                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                  <Feather style={styles.icon} name="trash" />
+                </TouchableOpacity>
+              </View>
+            </TouchableOpacity>
           )
         }}
       />
     </View>
   );
+};
+
+
+
+IndexScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => (
+      <View style={styles.plusIcon}>
+        <TouchableOpacity onPress={() => navigation.navigate('Create')}>
+          <Feather name="plus" size={30} />
+        </TouchableOpacity>
+      </View>
+    ),
+  };
 };
 
 const styles = StyleSheet.create({
@@ -39,7 +54,11 @@ const styles = StyleSheet.create({
   },
 
   title: {
-    fontSize:18
+    fontSize: 18
+  },
+
+  plusIcon: {
+    paddingRight: 10
   },
 
   icon: {
